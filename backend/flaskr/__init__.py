c6828c9b1c4c5b3bd38ef67b3ce0addf2fd2e9ad
Sinduja Raghavendra <sindujak29@gmail.com>
2020-06-10 00:23:35 -0700

    [feat]: Implementing Endpoints for adding questions, playing quiz and displaying a list of questions based on the category selected
    Details:
    1. Endpoint for adding question (POST request)
    2. Endpoint for playing quiz (POST request)
    3. Endpoint for listing questions based on the category selected (GET request)
    
    Resolves:
     * Adding new question to the database
     * Picking a random question from the database
     * Ability to list the questions based on category
    
    Tests:
     * By running App on localhost
    
    Signed-off-by: Sinduja Raghavendra <sindujak29@gmail.com>

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -75,16 +75,39 @@ def create_app(test_config=None):
   This removal will persist in the database and when you refresh the page. 
   '''
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
+  @app.route('/questions', methods=['POST'])
+  def add_questions():
+    body = request.get_json()
+    new_question = body.get('question', None)
+    new_answer = body.get('answer', None)
+    new_difficulty = body.get('difficulty', None)
+    new_category = body.get('category', None)
+
+    try:
+      question = Question(question=new_question, answer=new_answer, difficulty=new_difficulty, category=new_category)
+      question.insert()
+
+      questions = Question.query.order_by(Question.id).all()
+      total_questions = len(questions)
+      current_questions = paginate_questions(request, questions)
+
+      categories = Category.query.all()
+      categories_dict = {}
+      for category in categories:
+          categories_dict[category.id] = category.type
+
+      if (len(current_questions) == 0):
+          abort(404)
+
+      return jsonify({
+          'success': True,
+          'questions': current_questions,
+          'total_questions': total_questions,
+          'categories': categories_dict
+        })
+    except:
+        abort(422)
 
   '''
   @TODO: 
@@ -97,34 +120,97 @@ def create_app(test_config=None):
   Try using the word "title" to start. 
   '''
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
+  @app.route('/categories/<int:category_id>/questions')
+  def get_questions_based_on_category(category_id):
+    category = Category.query.filter_by(id=category_id).one_or_none()
+    if (category is None):
+      abort(400)
 
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
+    questions = Question.query.filter(Question.category == category_id).order_by(Question.id).all()
+    total_questions = len(questions)
+    current_questions = paginate_questions(request, questions)
+
+    categories = Category.query.all()
+    categories_dict = {}
+    for category in categories:
+        categories_dict[category.id] = category.type
+
+    if (len(current_questions) == 0):
+        abort(404)
+
+    return jsonify({
+        'success': True,
+        'questions': current_questions,
+        'total_questions': total_questions,
+        'categories': categories_dict,
+        'current_category': category.type
+      })
 
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
+  @app.route('/quizzes', methods=['POST'])
+  def get_questions_for_quiz():
+    body = request.get_json()
+    category = body.get('quiz_category', None)
+    previous = body.get('previous_questions')
+    try:
+      if category['id'] == 0:
+        questions = Question.query.all()
+      else:
+        questions = Question.query.filter(Question.category == category['id']).all()
 
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
+      totalQuestions = len(questions) 
 
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-  
+      def get_random_question():
+        return questions[random.randrange(0, len(questions), 1)]
+
+      def check_if_used(question):
+        used = False
+        for q in previous:
+            if (q == question.id):
+                used = True
+        return used
+
+      question = get_random_question()
+
+      while (check_if_used(question)):
+        question = get_random_question()
+
+        if (len(previous) == totalQuestions):
+          return jsonify({
+              'success': True
+          })
+
+      return jsonify({
+        'success': True,
+        'question': question.format()
+      })
+    except:
+        abort(422)
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          "success": False, 
+          "error": 404,
+          "message": "resource not found"
+      }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          "success": False, 
+          "error": 422,
+          "message": "unprocessable"
+      }), 422
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          "success": False, 
+          "error": 400,
+          "message": "bad request"
+      }), 400
+
   return app
 
     
--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 import '../stylesheets/FormView.css';
 
 class FormView extends Component {
-  constructor(props){
+  constructor(props) {
     super();
     this.state = {
       question: "",
@@ -15,12 +15,12 @@ class FormView extends Component {
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     $.ajax({
       url: `/categories`, //TODO: update request URL
       type: "GET",
       success: (result) => {
-        this.setState({ categories: "jf" })
+        this.setState({ categories: result.categories })
         return;
       },
       error: (error) => {
@@ -50,6 +50,7 @@ class FormView extends Component {
       crossDomain: true,
       success: (result) => {
         document.getElementById("add-question-form").reset();
+        document.getElementById('success').className = 'modal';
         return;
       },
       error: (error) => {
@@ -59,26 +60,35 @@ class FormView extends Component {
     })
   }
 
+  close = (event) => {
+    document.getElementById('success').className = 'hidden';
+  }
+
   handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value})
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
     return (
       <div id="add-form">
+        <div id="success" class='hidden'>Added question successfuly!!
+            <a> &nbsp; </a>
+            <button class="close" data-dismiss="alert" onClick={this.close}> &times;</button>
+        </div>
         <h2>Add a New Trivia Question</h2>
         <form className="form-view" id="add-question-form" onSubmit={this.submitQuestion}>
-          <label>
+          <label class="label">
             Question
-            <input type="text" name="question" onChange={this.handleChange}/>
+            <input type="text" name="question" onChange={this.handleChange} />
           </label>
-          <label>
+          <label class="label">
             Answer
-            <input type="text" name="answer" onChange={this.handleChange}/>
+            <input type="text" name="answer" onChange={this.handleChange} />
           </label>
-          <label>
+          <label class="label">
             Difficulty
-            <select name="difficulty" onChange={this.handleChange}>
+            <div></div>
+            <select style={{ width: '100px', height: '20px' }} name="difficulty" onChange={this.handleChange}>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -86,14 +96,16 @@ class FormView extends Component {
               <option value="5">5</option>
             </select>
           </label>
-          <label>
+
+          <label class="label">
             Category
-            <select name="category" onChange={this.handleChange}>
+            <div></div>
+            <select style={{ width: '100px', height: '20px' }}  name="category" onChange={this.handleChange}>
               {Object.keys(this.state.categories).map(id => {
-                  return (
-                    <option key={id} value={id}>{this.state.categories[id]}</option>
-                  )
-                })}
+                return (
+                  <option key={id} value={id}>{this.state.categories[id]}</option>
+                )
+              })}
             </select>
           </label>
           <input type="submit" className="button" value="Submit" />
